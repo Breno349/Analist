deeda718aa2565df41d8dabd43be68494ef76899
Breno349 <123127632+Breno349@users.noreply.github.com>
2024-02-02 23:15:38 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,22 +1,25 @@
-const puppeteer = require("puppeteer-core")
-/*const express = require('express')
-const app = express()
+const express = require('express');
+const puppeteer = require('puppeteer');
+const fs = require('fs');
 
-app.get("/", (req, res) => {
-  
-})
+const app = express();
+const port = 3000;
 
-app.listen(8000,() => {
-  console.log("Servidor Iniciado...")
-})*/
+// Rota para pegar o conteúdo do site e salvar no diretório / do site local
+app.get('/', async (req, res) => {
+    try {
+        const browser = await puppeteer.launch();
+        const page = await browser.newPage();
+        await page.goto('https://internetgratis.vivo.com.br'); // URL do site que você deseja pegar o conteúdo
 
-async function startPu(){
-  const browser = await puppeteer.launch({
-    headless: true,
-    args: [
-      "--no-sandbox"
-    ]
-  });
-}
+        const content = await page.content(); // Obtém o conteúdo da página
+        res.send(content)
+        await browser.close();
+    } catch (Exception e){
+      console.log("Erro"+e)
+    }
+});
 
-startPu()
+app.listen(port, () => {
+    console.log(`Servidor Express rodando na porta ${port}`);
+});
